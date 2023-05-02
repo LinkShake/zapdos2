28f2196ae2c473dd2de5211e2a55bd08365b17d4
LinkShake <nicolacampo19@gmail.com>
2023-05-02 15:09:47 +0200

    Tried fixing fastify deployments

--- apps/server/api/serverless.ts
+++ apps/server/api/serverless.ts
@@ -13,8 +13,9 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import("../src/appHandler"));
+app.register(import("../src/app"));
 
+// @ts-ignore
 const handler = async (req, res) => {
   await app.ready();
   app.server.emit("request", req, res);
